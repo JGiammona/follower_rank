6f9cfac5ebb085eb75ed5a7da0a13650456ca931
JGiammona <jagiammona@gmail.com>
2022-07-04 19:43:22 -0700

    Add V2 directory and script

---
+++ V2/my_followers.py
@@ -0,0 +1,36 @@
+import tweepy
+import json
+
+# Variables that contains the credentials to access Twitter API
+BEARER_TOKEN = '<Insert Twitter Developer Bearer Token Here>'
+
+client = tweepy.Client(BEARER_TOKEN, wait_on_rate_limit=True)
+
+# the screen_name of the targeted user
+screen_name = "<Insert Twitter Username here>"
+
+# Lookup User ID associated to screen_name
+user_info = client.get_user(username=screen_name)
+user_id = user_info.data.id
+
+followers = []
+
+# Paginator object for followers query
+paginator = tweepy.Paginator(
+    client.get_users_followers,
+    id=user_id,
+    user_fields=["description"]
+)
+
+# Pagination loop
+try: 
+    for page in paginator:
+        follower_page_data = [{"id":user.id, "username":user.username, "desc":user.description} for user in page.data]
+        followers.extend(follower_page_data)
+except tweepy.errors.TooManyRequests as exc:
+    print('Rate limit!')
+
+print("Followers: " + str(len(followers)))
+with open('followers_data.json', 'w', encoding='utf-8') as f:
+    json.dump(followers, f, ensure_ascii=False, indent=4)
+
